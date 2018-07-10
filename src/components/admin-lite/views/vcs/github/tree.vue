7785210c165bdd94cfb0e415439c7659c1b59245
Rosco Pecoltran <rosco_pecoltran@msn.com>
2018-07-10 14:42:00 +0200

    add github tree mock

--- src/components/admin-lite/views/vcs/github/tree.vue
+++ src/components/admin-lite/views/vcs/github/tree.vue
@@ -2,7 +2,7 @@
   <div class="app-container">
     <el-input placeholder="Filter keyword" v-model="filterText" style="margin-bottom:30px;"></el-input>
     <el-tree class="filter-tree" 
-        :data="data2" 
+        :data="res" 
         :props="defaultProps" 
         default-expand-all 
         :filter-node-method="filterNode" 
@@ -20,7 +20,12 @@ export default {
     return {
       res: null,
       resLoading: true,
-      filterText: ''
+      filterText: '',
+      defaultProps: {
+        children: 'children',
+        size: 'size',
+        label: 'path'
+      }
     }
   },
   filters: {
@@ -45,13 +50,13 @@ export default {
       this.resLoading = true
       console.log('github-tree fetchData')
       getTree(this.treeQuery).then(response => {
-        this.res = response.data.tree
+        this.res = response.tree
         this.resLoading = false
       })
     },
     filterNode (value, data) {
       if (!value) return true
-      return data.label.indexOf(value) !== -1
+      return data.path.indexOf(value) !== -1
     }
   },
   watch: {
@@ -59,52 +64,5 @@ export default {
       this.$refs.tree2.filter(val)
     }
   }
-
-  /*
-  data () {
-    return {
-      filterText: '',
-      data2: [{
-        id: 1,
-        label: 'Level one 1',
-        children: [{
-          id: 4,
-          label: 'Level two 1-1',
-          children: [{
-            id: 9,
-            label: 'Level three 1-1-1'
-          }, {
-            id: 10,
-            label: 'Level three 1-1-2'
-          }]
-        }]
-      }, {
-        id: 2,
-        label: 'Level one 2',
-        children: [{
-          id: 5,
-          label: 'Level two 2-1'
-        }, {
-          id: 6,
-          label: 'Level two 2-2'
-        }]
-      }, {
-        id: 3,
-        label: 'Level one 3',
-        children: [{
-          id: 7,
-          label: 'Level two 3-1'
-        }, {
-          id: 8,
-          label: 'Level two 3-2'
-        }]
-      }],
-      defaultProps: {
-        children: 'children',
-        label: 'label'
-      }
-    }
-  }
-  */
 }
 </script>
